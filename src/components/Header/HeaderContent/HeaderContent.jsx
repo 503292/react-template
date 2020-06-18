6d1d9cba3e7ae561200689c1c0d972ff1388942a
kalysh_maxym <503292@ukr.net>
2020-06-18 17:01:41 +0300

    DONE _ Header view

--- src/components/Header/HeaderContent/HeaderContent.jsx
+++ src/components/Header/HeaderContent/HeaderContent.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import css from './HeaderContent.module.scss';
-import logo1 from '../../../assets/image/logo1.svg';
-// import logo2 from "../../../assets/image/logo2.svg";
+import logo1 from '../../../assets/image/header/logo1.svg';
+
+// import logo2 from "../../../assets/image/header/logo2.svg";
+import slide1 from '../../../assets/image/header/slide-01.svg';
+// import slide2 from "../../../assets/image/header/slide-02.svg";
 
 import { ReactComponent as Phone } from '../../../assets/image/phone.svg';
+import { ReactComponent as ArrowR } from '../../../assets/image/header/arrowR.svg';
+import { ReactComponent as ArrowL } from '../../../assets/image/header/arrowL.svg';
+import { ReactComponent as Circle } from '../../../assets/image/header/circle.svg';
+import logoComfort from '../../../assets/image/header/logo-comfort-life.svg';
 
 const HeaderContent = () => {
   return (
@@ -23,8 +30,25 @@ const HeaderContent = () => {
           </button>
         </div>
       </div>
-      <div className={css.centerHeader}>w</div>
-      <div className={css.bottomHeader}>w</div>
+      <div className={css.centerHeader}>
+        <img className={css.slide} src={slide1} alt="slide" />
+        <div className={css.wrapCenter}>
+          <p className={css.description}>Жилой комплекс КЛАБ РЕЗИДЕНС</p>
+          <h3 className={css.title}>Квартиры уже в продаже!</h3>
+          <button className={css.aboutBtn} type="button">
+            О КОМПЛЕКСЕ
+          </button>
+          <div className={css.wrapArrows}>
+            <Circle /> <Circle /> <ArrowL /> <ArrowR />
+          </div>
+
+          <div className={css.bottomHeader}>
+            <p className={css.adress}>Киев, переулок Радистов, 14а</p>
+            <p className={css.subway}>Лесная</p>
+          </div>
+        </div>
+      </div>
+      <img className={css.logoComfort} src={logoComfort} alt="logo comfort" />
     </div>
   );
 };
