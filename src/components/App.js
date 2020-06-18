fa90603329aba7f9a0102b99422d830391650609
kalysh_maxym <503292@ukr.net>
2020-06-18 11:37:54 +0300

    LOAD _ img from figma

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Header from './Header/Header';
-import Gallary from './Gallary/Gallary';
-import Planning from './Planning/Planning';
-import Documents from './Documents/Documents';
-import Footer from './Footer/Footer';
+// import Gallary from './Gallary/Gallary';
+// import Planning from './Planning/Planning';
+// import Documents from './Documents/Documents';
+// import Footer from './Footer/Footer';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Gallary />
+      {/* <Gallary />
       <Planning />
       <Documents />
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
