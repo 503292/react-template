bd4623f3c0fa6639d6aff6c45ce078e14712fcea
kalysh_maxym <503292@ukr.net>
2020-06-19 16:33:56 +0300

    ADD _ slider in Gallary / ADD _ view Gallary / ADD _ logic Gallary

--- src/components/Gallary/Gallary.jsx
+++ src/components/Gallary/Gallary.jsx
@@ -6,42 +6,48 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import { ReactComponent as ArrowL } from '../../assets/image/header/arrowL.svg';
 import { ReactComponent as ArrowR } from '../../assets/image/header/arrowR.svg';
 
+import nextFoto from '../../assets/image/gallary/next-foto.png';
+import topFoto from '../../assets/image/gallary/bg-gallary.png';
+
 import css from './Gallary.module.scss';
 
-const imageOne =
-  'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg';
-const imageTwo =
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQaI53zIjVCmIWLcM341j3dnjVTOK-ypoBARIT8qZMLGi8IDuua&usqp=CAU';
-const imageThree =
-  'https://www.zenit.photo/upload/iblock/260/foto_aleksey_lukyanenko_zenitar_16.jpg';
-
-// const settings = {
-//   slides: [
-//     <img src={imageOne} />,
-//     <img src={imageTwo} />,
-//     <img src={imageThree} />,
-//   ],
-// };
+// const imageOne =
+//   'https://bigpicture.ru/wp-content/uploads/2015/11/nophotoshop29-800x532.jpg';
+// const imageTwo =
+//   'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQaI53zIjVCmIWLcM341j3dnjVTOK-ypoBARIT8qZMLGi8IDuua&usqp=CAU';
+// const imageThree =
+//   'https://www.zenit.photo/upload/iblock/260/foto_aleksey_lukyanenko_zenitar_16.jpg';
 
 class Gallary extends Component {
   state = {
     value: 0,
     slides: [
-      <img src={imageOne} />,
-      <img src={imageTwo} />,
-      <img src={imageThree} />,
+      <img src={topFoto} key="1" alt="randomImage" />,
+      <img src={topFoto} key="2" alt="randomImage" />,
+      <img src={topFoto} key="3" alt="randomImage" />,
+      <img src={topFoto} key="4" alt="randomImage" />,
+      <img src={topFoto} key="5" alt="randomImage" />,
     ],
     thumbnails: [
-      <img src={imageOne} />,
-      <img src={imageTwo} />,
-      <img src={imageThree} />,
+      <img src={topFoto} key="1" alt="randomImage" />,
+      <img src={topFoto} key="2" alt="randomImage" />,
+      <img src={topFoto} key="3" alt="randomImage" />,
+      <img src={topFoto} key="4" alt="randomImage" />,
+      <img src={topFoto} key="5" alt="randomImage" />,
     ],
+    isActiveBtn: true,
   };
 
   onChange = value => this.setState({ value });
 
+  onClickBtn = e => {
+    e.preventDefault();
+    const { isActiveBtn } = this.state;
+    this.setState({ isActiveBtn: !isActiveBtn });
+  };
+
   render() {
-    const { value, slides, thumbnails } = this.state;
+    const { value, slides, thumbnails, isActiveBtn } = this.state;
 
     return (
       <section className={css.wpapGallary}>
@@ -53,9 +59,11 @@ class Gallary extends Component {
           onChange={this.onChange}
         >
           {/* {settings.slides.map(el => )} */}
-          <img src={imageOne} className={css.carouselImg} alt="randomImage" />
-          <img src={imageTwo} className={css.carouselImg} alt="randomImage" />
-          <img src={imageThree} className={css.carouselImg} alt="randomImage" />
+          <img src={topFoto} className={css.carouselImg} alt="randomImage" />
+          <img src={topFoto} className={css.carouselImg} alt="randomImage" />
+          <img src={topFoto} className={css.carouselImg} alt="randomImage" />
+          <img src={topFoto} className={css.carouselImg} alt="randomImage" />
+          <img src={topFoto} className={css.carouselImg} alt="randomImage" />
         </Carousel>
         <Dots
           number={thumbnails.length}
@@ -64,47 +72,48 @@ class Gallary extends Component {
           onChange={this.onChange}
           className={css.thumbnails}
         />
-        {/* <div> */}
+
         <Dots
           value={value}
           onChange={this.onChange}
           number={slides.length}
           className={css.dots}
         />
-        {/* </div> */}
+
+        <div className={css.wrapControls}>
+          <div>
+            <button
+              onClick={this.onClickBtn}
+              type="button"
+              className={
+                isActiveBtn
+                  ? `${css.switchBtn} ${css.active}`
+                  : `${css.switchBtn}`
+              }
+              active={isActiveBtn}
+            >
+              КЛУБНЫЙ ДОМ
+            </button>
+            <button
+              onClick={this.onClickBtn}
+              type="button"
+              className={
+                !isActiveBtn
+                  ? `${css.switchBtn} ${css.active}`
+                  : `${css.switchBtn}`
+              }
+            >
+              КЛАБ РЕЗИДЕНС
+            </button>
+            <div className={css.wrapArrows}>
+              <ArrowL />
+              <ArrowR />
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
 }
 
 export default Gallary;
-
-// const Gallary = () => {
-//   const [value, setValue] = useState(0);
-
-//   const handlerChange = val => {
-//     setValue(val);
-//     console.log(val);
-//   };
-
-//   return (
-// <section className={css.wpapGallary}>
-//   <div className={css.leftBar}>f</div>
-//   <Carousel className={css.carousel}>
-//     {/* {settings.slides.map(el => )} */}
-//     <img src={imageOne} className={css.carouselImg} alt="randomImage" />
-//     <img src={imageTwo} className={css.carouselImg} alt="randomImage" />
-//     <img src={imageThree} className={css.carouselImg} alt="randomImage" />
-//   </Carousel>
-
-//   <Dots
-//     value={value}
-//     onChange={() => handlerChange(value)}
-//     number={settings.slides.length}
-//     className={css.dots}
-//   />
-// </section>
-//   );
-// };
-
-// export default Gallary;
